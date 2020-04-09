759fd969d84d93aaa7a118b1d7e6bb3272e5f2a3
xiaocai2333 <2363080731@qq.com>
2020-04-09 20:03:55 +0800

    update data

--- get_china_geocoord.py
+++ get_china_geocoord.py
@@ -2,12 +2,12 @@
 import json
 import requests
 
-my_api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+my_api_key = "d93cf23be9f207f17d4bfe48619f386d"
 
 
 def get_city_geo_coord():
     geo_coord_result = {}
-    with open("COVID-china-330.json", "r") as json_file:
+    with open("./DingXiang-COVID-data/COVID-china-data/COVID-china-2020-04-09.json", "r") as json_file:
         data = json.load(json_file)["data"]
         for i in range(len(data)):
             province_data = data[i]
@@ -20,38 +20,35 @@ def get_city_geo_coord():
                 result = requests.request('GET', url)
                 result = result.json()
                 if result["status"] == '0' or result["count"] == '0':
-                    longitude = "null"
-                    latitude = "null"
+                    continue
                 else:
                     longitude = result["geocodes"][0]["location"].split(",")[0]
                     latitude = result["geocodes"][0]["location"].split(",")[1]
                 geo_coord_result[city_name] = [longitude, latitude]
 
-    with open("./china_city_geo_coord.json", "w") as f:
-        json.dump(geo_coord_result, f, indent=4, sort_keys=True)
+    with open("./china_city_geo_coord.json", "w", encoding='utf-8') as f:
+        json.dump(geo_coord_result, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 
 def get_province_geo_coord():
     geo_coord_result = {}
-    with open("COVID-china-330.json", "r") as json_file:
+    with open("./DingXiang-COVID-data/COVID-china-data/COVID-china-2020-04-09.json", "r") as json_file:
         data = json.load(json_file)["data"]
         for i in range(len(data)):
             province_data = data[i]
             province_chinese_name = province_data["provinceName"]
-            province_name = province_data["provinceEnglishName"]
             url = "https://restapi.amap.com/v3/geocode/geo?key=" + my_api_key + "&address=" + province_chinese_name
             result = requests.request('GET', url)
             result = result.json()
             if result["status"] == '0' or result["count"] == '0':
-                longitude = "null"
-                latitude = "null"
+                continue
             else:
                 longitude = result["geocodes"][0]["location"].split(",")[0]
                 latitude = result["geocodes"][0]["location"].split(",")[1]
-            geo_coord_result[province_name] = [longitude, latitude]
+            geo_coord_result[province_chinese_name] = [longitude, latitude]
 
-    with open("./china_province_geo_coord.json", "w") as f:
-        json.dump(geo_coord_result, f, indent=4, sort_keys=True)
+    with open("./china_province_geo_coord.json", "w", encoding='utf-8') as f:
+        json.dump(geo_coord_result, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
