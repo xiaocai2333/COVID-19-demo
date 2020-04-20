ad7f8d40dd9b5296479020480352caa38d39fb03
xiaocai2333 <2363080731@qq.com>
2020-04-20 16:57:17 +0800

    add update every day

--- update_everyday.sh
+++
@@ -1,6 +0,0 @@
-cd /home/zc/work/COVID-19
-git pull
-cd /home/zc/work/COVID-19-demo
-python3 scripts/get_COVID_data.py
-python3 scripts/collect_DingXiang_data.py
-python3 scripts/collect_country_data.py
